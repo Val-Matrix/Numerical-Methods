158435b17540cb2486884459e3a66da9e2f6e822
Val-Matrix <A176695@siswa.ukm.edu.my>
2022-01-08 18:37:52 +0800

    Update option for user input configuration.

--- LinearEqn.cpp
+++ LinearEqn.cpp
@@ -7,39 +7,71 @@ int main()
 {
 	double mult, sum;
 	int i, j, k, n = SIZE;
+	double a[SIZE][SIZE], b[SIZE], x[SIZE];
+	char choice;
 
-	double a[SIZE][SIZE];
-	double b[SIZE] = { 1400, 100, 2000, 0 };
-	double x[SIZE];
-
-	cout << "Please key in the coefficients for matrix A." << endl;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = 0; j < n; ++j)
+	cout << "LINEAR ALGEBRAIC EQUATION SOLVING USING NAIVE GAUSS ELIMINATION, Ax = B (4 EQUATIONS)" << endl;
+	
+	cout << "-------------------------------------------------------------------------------------" << endl;
+	do {
+		cout << "Please key in the coefficients for matrix A." << endl;
+		for (i = 0; i < n; ++i)
 		{
-			cout << "a[" << i << "][" << j << "] = ";
-			cin >> a[i][j];
-		}
-	}
-
-	cout << endl << "Continue with this matrix A?" << endl;
-	for (i = 0; i < n; ++i)
-	{
-		for (j = 0; j < n; ++j)
-		{
-			cout.width(9);
-			cout << a[i][j];
+			for (j = 0; j < n; ++j)
+			{
+				cout << "a[" << i << "][" << j << "] = ";
+				cin >> a[i][j];
+			}
 		}
 		cout << endl;
-	}
 
-	cout << "Please key in the coefficients for matrix B." << endl;
-	for (i = 0; i < n; ++i)
-	{
-		cout << "b[" << i << "] = ";
-		cin >> b[i];
-	}
-	
+		for (i = 0; i < n; ++i)
+		{
+			for (j = 0; j < n; ++j)
+			{
+				cout.width(9);
+				cout << a[i][j];
+			}
+			cout << endl;
+		}
+
+		do {
+			cout << "Continue with this matrix A? (Y/N): ";
+			cin >> choice;
+			if (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n')
+				cout << "Invalid input. ";
+		} while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
+		
+		cout << endl;
+	} while (choice == 'N' || choice == 'n');
+
+	cout << "------------------------------------------------" << endl;
+	do {
+		cout << "Please key in the coefficients for matrix B." << endl;
+		for (i = 0; i < n; ++i)
+		{
+			cout << "b[" << i << "] = ";
+			cin >> b[i];
+		}
+		cout << endl;
+
+		for (i = 0; i < n; ++i)
+		{
+			cout.width(9);
+			cout << b[i];
+			cout << endl;
+		}
+
+		do {
+			cout << "Continue with this matrix B? (Y/N): ";
+			cin >> choice;
+			if (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n')
+				cout << "Invalid input. ";
+		} while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
+		
+		cout << endl;
+	} while (choice == 'N' || choice == 'n');
+
 	// Forward elimination
 	for (k = 0; k < n; ++k)
 	{
--- NewtonRaph.cpp
+++ NewtonRaph.cpp
@@ -3,75 +3,150 @@
 using namespace std;
 const double NEARLY_ZERO = 1e-40;
 
-double f(double x);
-double df(double x);
+double f(double x, double a, double b, double c, double d);
+double df(double x, double a, double b, double c);
 
 int main()
 {
-	double x_init, epsilon, root;
-	double x_prev, x_curr, derf;
+	double epsilon = 1e-12, x_init, x_prev, x_curr, derf, root, a, b, c, d;
 	int num_iter, max_iter;
-	epsilon = 1e-12;
-	max_iter = 30;
+	char choice;
 
-	cout << "ROOTS OF AN EQUATION USING NEWTON-RAPHSON METHOD" << endl;
-	cout << "Please key in an initial guess: ";
-	cin >> x_init;
-	cout << "Initial guess for the root = " << x_init << endl;
-	num_iter = 0;
-	x_prev = x_init;
-	x_curr = x_init;
+	cout << "ROOTS OF AN CUBIC EQUATION USING NEWTON-RAPHSON METHOD" << endl;
+	cout << "------------------------------------------------------" << endl;
 	
-	while (num_iter < max_iter)
-	{
-		// Evaluate derivative of function
-		derf = df(x_prev);
-		if (fabs(derf) <= NEARLY_ZERO)
+	do {
+		cout << "Please key in the coefficients for the function f(x) = ax^3 + bx^2 + cx + d." << endl;
+		cout << "a = ";
+		cin >> a;
+		cout << "b = ";
+		cin >> b;
+		cout << "c = ";
+		cin >> c;
+		cout << "d = ";
+		cin >> d;
+
+		cout << endl << "f(x) = ";
+		if (a == -1)
+			cout << "- " << "x^3 ";
+		else if (a < 0)
+			cout << "- " << fabs(a) << "x^3 ";
+		else if (a == 1)
+			cout << "x^3 ";
+		else if (a > 0)
+			cout << a << "x^3 ";
+
+		if (b == -1)
+			cout << "- " << "x^2 ";
+		else if (b < 0)
+			cout << "- " << fabs(b) << "x^2 ";
+		else if (b == 1)
+			cout << "+ x^2 ";
+		else if (b > 0)
+			cout << "+ " << b << "x^2 ";
+
+		if (c == -1)
+			cout << "- " << "x ";
+		else if (c < 0)
+			cout << "- " << fabs(c) << "x ";
+		else if (c == 1)
+			cout << "+ x ";
+		else if (c > 0)
+			cout << "+ " << c << "x ";
+
+		if (d < 0)
+			cout << "- " << fabs(d);
+		else if (d > 0)
+			cout << "+ " << d;
+
+		if (a == 0 && b == 0 && c == 0 && d == 0)
+			cout << "0";
+
+		do {
+			cout << endl << "Continue with this function? (Y/N): ";
+			cin >> choice;
+			if (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n')
+				cout << "Invalid input. ";
+		} while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
+
+		cout << endl;
+	} while (choice == 'N' || choice == 'n');
+	
+	do {
+		cout << "Please key in an initial guess: ";
+		cin >> x_init;
+		cout << "Please key in the maximum number of iterations: ";
+		cin >> max_iter;
+
+		cout << endl << "----------------------------------------------------" << endl;
+		cout << "Initial guess for the root = " << x_init << endl;
+		num_iter = 0;
+		root = x_curr = x_prev = x_init;
+
+		while (num_iter < max_iter)
 		{
-			cout << "Newton-Raphson method did not converge." << endl;
-			cout << "The derivative is close to zero." << endl;
-			cout << "The current estimate of the root = " << root << endl;
-			cout << "Value of the function at this estimate, f(" << root << ") = " << f(root) << endl;
-			cout << "Number of iterations = " << num_iter << endl;
+			// Evaluate derivative of function
+			derf = df(x_prev, a, b, c);
+			if (fabs(derf) <= NEARLY_ZERO)
+			{
+				cout << "Newton-Raphson method did not converge." << endl;
+				cout << "The derivative is close to zero." << endl;
+				cout << "The current estimate of the root = " << root << endl;
+				cout << "Value of the function at this estimate, f(" << root << ") = " << f(root, a, b, c, d) << endl;
+				cout << "Number of iterations = " << num_iter << endl;
+				cout << "----------------------------------------------------" << endl;
+			}
+
+			// Compute new estimate of the root
+			x_curr = x_prev - f(x_prev, a, b, c, d) / derf;
+			++num_iter;
+			root = x_curr;
+
+			// Check for convergence
+			if (fabs(x_curr - x_prev) <= fabs(x_curr * epsilon)) // Eq. 2
+			{
+				cout << "The root of the equation = " << root << endl;
+				cout << "Value of the function at the root, f(" << root << ") = " << f(root, a, b, c, d) << endl;
+				cout << "Number of iterations = " << num_iter << endl;
+				cout << "----------------------------------------------------" << endl;
+				break;
+			}
+
+			// Update
+			x_prev = x_curr;
 		}
 
-		// Compute new estimate of the root
-		x_curr = x_prev - f(x_prev) / derf;
-		++num_iter;
-		root = x_curr;
-
-		// Check for convergence
-		if (fabs(x_curr - x_prev) <= fabs(x_curr * epsilon)) // Eq. 2
+		if (num_iter == max_iter)
 		{
-			cout << "The root of the equation = " << root << endl;
-			cout << "Value of the function at the root, f(" << root << ") = " << f(root) << endl;
-			cout << "Number of iterations = " << num_iter << endl;
-			break;
+			cout << "Did not converge after " << max_iter << " iterations." << endl;
+			cout << "Current estimate of the root = " << root << endl;
+			cout << "Value of the function at the root, f(" << root << ") = " << f(root, a, b, c, d) << endl;
+			cout << "----------------------------------------------------" << endl;
 		}
 
-		// Update
-		x_prev = x_curr;
-	}
+		do {
+			cout << endl << "Repeat with different initial guess? (Y/N): ";
+			cin >> choice;
+			if (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n')
+				cout << "Invalid input. ";
+		} while (choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n');
 
-	if (num_iter == max_iter)
-	{
-		cout << "Did not converge after " << max_iter << " iterations." << endl;
-		cout << "Current estimate of the root = " << root << endl;
-		cout << "Value of the function at the root, f(" << root << ") = " << f(root) << endl;
-	}
+		cout << endl;
+	} while (choice == 'Y' || choice == 'y');	
+	
 	return 0;
 }
 
-double f(double x)
+double f(double x, double a, double b, double c, double d)
 {
 	double fx;
-	fx = 0.5 * x * x * x - 4 * x * x + 6 * x - 2;
+	fx = a * x * x * x + b * x * x + c * x + d;
 	return(fx);
 }
 
-double df(double x)
+double df(double x, double a, double b, double c)
 {
 	double dfx;
-	dfx = 1.5 * x * x - 8 * x + 6;
+	dfx = 3 * a * x * x + 2 * b * x + c;
 	return(dfx);
 }
